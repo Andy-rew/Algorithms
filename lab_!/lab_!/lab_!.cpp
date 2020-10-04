46791b9cc0f1cb92c1c12f83a633cb88cd9f7eee
Andrew Ch <63413425+Andy-rew@users.noreply.github.com>
2020-10-05 00:53:08 +0300

    List

---
+++ lab_!/lab_!/lab_!.cpp
@@ -0,0 +1,133 @@
+﻿
+#include <iostream>
+
+using namespace std;
+//динамический массив
+void ArrayList()
+{
+    int n;
+    cin >> n;
+    int* mas = new int [n]; 
+    delete[] mas;
+}
+//односвязный список
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+class List
+{
+public:
+	List();
+	~List();
+    void push_back(int data);
+    void push_front(int data);
+    void print();
+    void insert(int data, int index);
+    void pop_front();
+    void clear();
+    int GetSize()
+    {
+        return size;
+    }
+private:
+    Node* head;
+    int size;
+};
+
+List::List()
+{
+    head = NULL;
+    size = 0;
+}
+
+List::~List()
+{
+    clear();
+}
+
+void List::push_back(int data)
+{
+    Node* nd = new Node;
+    nd->data = data;
+    nd->next = NULL;
+    if (head == NULL)
+        head = nd;
+    else
+    {
+        Node* current = head;
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = nd;
+    }
+    size++;
+}
+
+void List::push_front(int data)
+{
+    Node* nd = new Node;
+    nd->data = data;
+    nd->next = head;
+    head = nd;
+    size++;
+}
+
+void List::print()
+{
+    Node* current = head;
+    while (current != NULL)
+    {
+        cout << current->data << endl;
+        current = current->next;
+    }
+}
+
+void List::insert(int data, int index)
+{
+    if (index == 0)
+    {
+        push_front(data);
+    }
+    else
+    {
+        Node* nd = new Node;
+        Node* current = head;
+        nd->data = data;
+        for (int i = 0; i < index - 1; ++i)
+        {
+            current = current->next;
+        }
+        nd->next = current->next;
+        current->next = nd;
+        size++;
+    }
+}
+
+void List::pop_front()
+{
+    Node* tmp = head;
+    head = head->next;
+    size--;
+    delete tmp;
+}
+
+void List::clear()
+{
+    while (size != 0)
+    {
+        pop_front();
+    }
+}
+//стек - урезанный список. Операции: добавление элемента в начало списка, удаление первого элемента
+
+int main()
+{
+   
+	system("pause");
+	return 0;
+}
+
